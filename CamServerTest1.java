94f6b5dd6cce4fa3bec432c0103747a1c95ba938
Allan (Slam) <72238104+aayes89@users.noreply.github.com>
2023-07-06 15:15:26 -0600

    Update CamServerTest1.java

--- CamServerTest1.java
+++ CamServerTest1.java
@@ -24,24 +24,24 @@ public class MyCam {
     public static void main(String[] args) {
         desiredMacAddress.put("00:1e:b5:84:8f:01", Boolean.FALSE);
         desiredMacAddress.put("f8:da:c:7d:e9:2f", Boolean.FALSE); // Dirección MAC deseada Ever Sparkle Technologies Ltd
-        /*String ip = "192.168.1.43";
-        int[] ports = {/*8070,8080,*/12476, /*19122, 21664,24188,*/ 32108};
+        //String ip = "192.168.1.43";
+        //int[] ports = {/*8070,8080,8090,*/12476, /*19122, 21664,24188,*/ 32108};
         //sendData(hexToAscii("5d782818"), "10.10.1.43", 32108);
-        for (int port : ports) {
-            System.out.println("\nProbing on port: " + port);
-            sendData(hexToAscii("5d099800ed958ebe07b243c05d4fcb7bb76849e0a3898058"), ip, port);
-            sendData(hexToAscii(StrimHexCad("5d 98 14 de 18 6a 52 36  fc fc f6 5f f6 5f b1 c339 9d 9b 85 b0 57 c0 38  89 eb 3f d6 27 64 e5 3baf c1 48 43 ac 31 b2 2a  fb e8 a1 29 27 60 6c afcf 91 71 63 fb fa 60 66a42642e20e902256f74ea5e1dfba1fa0afcbefbdf8c02af1c31fa5ff7e53bd")), ip, port);
-            sendData(hexToAscii("5d99e6bbfdb5444738ea"), ip, port);
-            sendData(hexToAscii("5d99e6bbfdb5444738eb"), ip, port);
-            sendData(hexToAscii("5da80ef4"), ip, port);
-            sendData(hexToAscii("5da96394"), ip, port);
-            sendData("ࣉ�", ip, port);
-            sendData(hexToAscii("5da96394"), ip, port);
-            sendData(hexToAscii("5d9814f42a95cdf51a21ddeb0d2a95cdb0715b69b1afcf89e566a0b9f9ca91790feb09a756eda4225ea74c779412feb623c85b2c0cd5e8b7133f95fbbb4a043dc31b7289f49eba1339fefc99813a3fd62764aacbe202709406454b0139b635474b5b287c37fa6737e6ce5b3bf19f1d836b9766a43056a426558d79023f"), ip, port);
-            sendData(hexToAscii("5d99e6b112cfff130cb4ec83021b00af"), ip, port);
-            sendData(hexToAscii("5d099800ed958ebe07b243c05d4fcb7bb76849e0a3898058"), ip, port);
-        }
-        System.exit(0);*/
+        //for (int port : ports) {
+        //    System.out.println("\nProbing on port: " + port);
+        //    sendData(hexToAscii("5d099800ed958ebe07b243c05d4fcb7bb76849e0a3898058"), ip, port);
+        //    sendData(hexToAscii(StrimHexCad("5d 98 14 de 18 6a 52 36  fc fc f6 5f f6 5f b1 c339 9d 9b 85 b0 57 c0 38  89 eb 3f d6 27 64 e5 3baf c1 48 43 ac 31 b2 2a  fb e8 a1 29 27 60 6c afcf 91 71 63 fb fa 60 66a42642e20e902256f74ea5e1dfba1fa0afcbefbdf8c02af1c31fa5ff7e53bd")), ip, port);
+        //    sendData(hexToAscii("5d99e6bbfdb5444738ea"), ip, port);
+        //    sendData(hexToAscii("5d99e6bbfdb5444738eb"), ip, port);
+        //    sendData(hexToAscii("5da80ef4"), ip, port);
+        //    sendData(hexToAscii("5da96394"), ip, port);
+        //    sendData("ࣉ�", ip, port);
+        //    sendData(hexToAscii("5da96394"), ip, port);
+        //    sendData(hexToAscii("5d9814f42a95cdf51a21ddeb0d2a95cdb0715b69b1afcf89e566a0b9f9ca91790feb09a756eda4225ea74c779412feb623c85b2c0cd5e8b7133f95fbbb4a043dc31b7289f49eba1339fefc99813a3fd62764aacbe202709406454b0139b635474b5b287c37fa6737e6ce5b3bf19f1d836b9766a43056a426558d79023f"), ip, port);
+        //    sendData(hexToAscii("5d99e6b112cfff130cb4ec83021b00af"), ip, port);
+        //    sendData(hexToAscii("5d099800ed958ebe07b243c05d4fcb7bb76849e0a3898058"), ip, port);
+        //}
+        //System.exit(0);
 
         Scanner in = new Scanner(System.in);
         while (true) {
