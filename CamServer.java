cc27566fa50fac99c53782d87d8b35039e1a7438
Allan (Slam) <72238104+aayes89@users.noreply.github.com>
2023-07-11 21:56:03 -0600

    Update CamServer.java

--- CamServer.java
+++ CamServer.java
@@ -11,8 +11,6 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class JCamara {
 
